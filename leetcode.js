ee134e7f78539cb3f0ae16886eabef2948bd1391
poojan124 <poojan124@hotmail.com>
2019-04-21 18:48:00 -0400

    better code :)

--- leetcode.js
+++ leetcode.js
@@ -1,24 +1,9 @@
-var foo = function(){
-    var l = document.getElementsByClassName('label');
-    var easy = document.getElementsByClassName('css-14oi08n');
-    var medium = document.getElementsByClassName('css-dcmtd5');
-    var hard = document.getElementsByClassName('css-t42afm');
-	for(var i = 0;i<l.length;i++){
-        l[i].style.display = 'none';	
-	}
-	if(medium.length >0){
-		medium[0].style.display = 'none';
-	}
-	if(easy.length >0){
-		easy[0].style.display = 'none';
-	}
-	if(hard.length >0){
-		hard[0].style.display = 'none';
-	}  
+var inject = function(css_code){
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css_code;
+    document.getElementsByTagName('head')[0].appendChild(style);
 };
-
-for(var i =0;i<100;i++){
-    window.setTimeout(foo,100*i);
-}
-
+var code = "span.label, div.css-14oi08n, div.css-dcmtd5, div.css-t42afm {display:none}"
+inject(code);
 
