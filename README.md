052cc759ba819b61bebd7a87dab77f5bfb469949
poojan124 <poojan124@hotmail.com>
2019-04-21 17:32:34 -0400

    Added Readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# LeetCode-difficulty-Hider
+* Have you ever feel like knowing the diffuculty of the problem affects you thinking? 
+* Sometime one will get intimidated by problem knowing its difficulty.
+* This chrome extension will hide diffuclty of a problem from leetcode.
+
+### How to Use this?
+* clone/download this repo. (unzip)
+* Go to chrome Extension 
+* Turn on developer mode on right hand side.
+* press Load Unpacked from top left side and select unzipped folder.
+* Done!
+
+### TODO:
+* [x] hide difficulties.
+* [ ] button to toggle between showing and hiding
+* [ ] button to show difficulty for current problem only (https://leetcode.com/problems/two-sum/) 
--- leetcode.js
+++ leetcode.js
@@ -1,18 +1,21 @@
 var foo = function(){
     var l = document.getElementsByClassName('label');
+    var easy = document.getElementsByClassName('css-14oi08n');
+    var medium = document.getElementsByClassName('css-dcmtd5');
+    var hard = document.getElementsByClassName('css-t42afm');
 	for(var i = 0;i<l.length;i++){
         l[i].style.display = 'none';	
 	}
-	if(document.getElementsByClassName('css-dcmtd5').length >0){
-		document.getElementsByClassName('css-dcmtd5')[0].style.display = 'none';
+	if(medium.length >0){
+		medium[0].style.display = 'none';
 	}
-	if(document.getElementsByClassName('css-14oi08n').length >0){
-		document.getElementsByClassName('css-14oi08n')[0].style.display = 'none';
+	if(easy.length >0){
+		easy[0].style.display = 'none';
 	}
-	if(document.getElementsByClassName('css-t42afm').length >0){
-		document.getElementsByClassName('css-t42afm')[0].style.display = 'none';
+	if(hard.length >0){
+		hard[0].style.display = 'none';
 	}  
-}
+};
 
 for(var i =0;i<100;i++){
     window.setTimeout(foo,100*i);
